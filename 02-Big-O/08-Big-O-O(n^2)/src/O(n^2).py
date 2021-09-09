5356a7fa38af6b899d8cd0472ee0fa854bb5c655
covuworie <chuk.ovuworie@gmail.com>
2021-09-09 18:18:57 -0500

    Upload correct code files for lession 8.

--- 02-Big-O/08-Big-O-O(n^2)/src/O(n^2).py
+++ 02-Big-O/08-Big-O-O(n^2)/src/O(n^2).py
@@ -1,9 +1,6 @@
 def print_items(n: int) -> None:
     for i in range(n):
         for j in range(n):
-            print(i,j)
-    
-    for k in range(n):
-        print(k)
+            print(i,j) 
 
 print_items(10)
