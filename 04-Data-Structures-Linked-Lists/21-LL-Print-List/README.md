99918666172fde9b1e3d5657c301d2c6c7f92435
covuworie <chuk.ovuworie@gmail.com>
2021-09-14 20:27:12 -0500

    21. LL - Print List

---
+++ 04-Data-Structures-Linked-Lists/21-LL-Print-List/README.md
@@ -0,0 +1,3 @@
+# 21. LL: Print List
+
+The code defines a method `print_list` that prints from head to tail each of the values in a **LinkedList**.
---
+++ 04-Data-Structures-Linked-Lists/21-LL-Print-List/src/LL-Print_List.py
@@ -0,0 +1,41 @@
+from typing import Optional
+
+class Node:
+    def __init__(self, value: int) -> None:
+        self.value = value
+        self.next: Optional[int] = None
+        
+
+class LinkedList:
+    def __init__(self, value: int) -> None:
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self) -> None:
+        node = self.head
+        while node is not None:
+            print(node.value)
+            node = node.next
+        
+    def append(self, value: int) -> bool:
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+
+my_linked_list = LinkedList(11)
+my_linked_list.append(3)
+my_linked_list.append(23)
+my_linked_list.append(7)
+
+my_linked_list.print_list()
+
+
