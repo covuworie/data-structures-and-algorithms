bbf30a2c511fac6741c239f1e81e770f5f36256f
covuworie <chuk.ovuworie@gmail.com>
2021-09-19 14:07:44 -0500

    27. LL - Get

---
+++ 04-Data-Structures-Linked-Lists/27-LL-Get/README.md
@@ -0,0 +1,11 @@
+# 27. LL: Get
+
+The LinkedList get method takes an index as its parameter and returns the node at that index. 
+
+From the code, we can see that we must first test that we have a valid index within the bounds of the length of the list and return None in this situation.
+
+![Linked List Get Invalid](./images/linked-list-get-invalid.jpg?raw=true "Linked List Get Invalid")
+
+Then the code deals with the valid case where the index is within the bounds of the list. It points temp to the first node and then loops through the list using a for loop with the index as its range.
+
+![Linked List Get](./images/linked-list-get.jpg?raw=true "Linked List Get")
---
+++ 04-Data-Structures-Linked-Lists/27-LL-Get/src/LL-Get.py
@@ -0,0 +1,86 @@
+from typing import Optional, Union
+
+class Node:
+    def __init__(self, value: int) -> None:
+        self.value = value
+        self.next: Optional[int] = None
+        
+
+class LinkedList:
+    def __init__(self, value: int) -> None:
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self) -> None:
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        
+    def append(self, value: int) -> bool:
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def pop(self) -> Union[Node, None]:
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while(temp.next):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+
+    def prepend(self, value: int) -> bool:
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+
+    def pop_first(self) -> Union[Node, None]:
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return temp
+
+    def get(self, index: int) -> Union[Node, None]:
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp.value  # for testing => should return the Node
+        
+
+my_linked_list = LinkedList(0)
+my_linked_list.append(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+
+print(my_linked_list.get(2))
+
