c7e6c4c6845c377bdafa8c1e93013d9464134a61
covuworie <chuk.ovuworie@gmail.com>
2021-09-30 20:36:28 -0500

    Remove superfluous line

--- 04-Data-Structures-Linked-Lists/31-LL-Reverse/src/LL-Reverse.py
+++ 04-Data-Structures-Linked-Lists/31-LL-Reverse/src/LL-Reverse.py
@@ -119,7 +119,6 @@ class LinkedList:
         temp = self.head
         self.head = self.tail
         self.tail = temp
-        after = temp.next
         before = None
         for _ in range(self.length):
             after = temp.next
