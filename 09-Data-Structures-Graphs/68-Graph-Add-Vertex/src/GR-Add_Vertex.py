18ebb623b97ca2f569bbe766aec45cdbc0498950
covuworie <chuk.ovuworie@gmail.com>
2021-11-10 18:43:13 -0600

    Update types

--- 09-Data-Structures-Graphs/68-Graph-Add-Vertex/src/GR-Add_Vertex.py
+++ 09-Data-Structures-Graphs/68-Graph-Add-Vertex/src/GR-Add_Vertex.py
@@ -1,14 +1,14 @@
-from typing import Dict, List
+from typing import Dict, List, Union
 
 class Graph:
     def __init__(self) -> None:
-        self.adj_list: Dict[str, List[str]] = {}
+        self.adj_list: Dict[Union[str, int], List[Union[str, int]]] = {}
 
     def print_graph(self) -> None:
         for vertex, edges in self.adj_list.items():
             print(f"{vertex}: {edges}")
 
-    def add_vertex(self, vertex: str) -> bool:
+    def add_vertex(self, vertex: Union[str, int]) -> bool:
         if vertex in self.adj_list:
             return False
         self.adj_list[vertex] = []
