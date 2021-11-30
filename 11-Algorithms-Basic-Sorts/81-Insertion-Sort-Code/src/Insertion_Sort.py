45533227b3c3b866f2ececa979edbb844abac2f2
covuworie <chuk.ovuworie@gmail.com>
2021-11-29 20:20:58 -0600

    Refactor the insertion sort

--- 11-Algorithms-Basic-Sorts/81-Insertion-Sort-Code/src/Insertion_Sort.py
+++ 11-Algorithms-Basic-Sorts/81-Insertion-Sort-Code/src/Insertion_Sort.py
@@ -12,18 +12,15 @@ def insertion_sort(my_list: List[int]) -> List[int]:
 
 # https://www.techiedelight.com/insertion-sort-iterative-recursive/
 def insertion_sort_recursive(my_list: List[int]) -> List[int]:
-    for i in range(1, len(my_list)):
-        sorted_set = my_list[:i+1]
-        unsorted_set = my_list[i+1:]
-        temp = my_list[i]
+    if len(my_list) == 1:
+        return my_list
 
-        for j in range(i - 1, -1, -1):
-            if sorted_set[j+1] >= sorted_set[j]:
-                break
-            sorted_set[j+1] = sorted_set[j]
-            sorted_set[j] = temp
-        my_list = sorted_set + insertion_sort_recursive(unsorted_set)
-    return my_list
+    for j in range(1, len(my_list)):
+        if my_list[j] < my_list[j-1]:
+            temp = my_list[j]
+            my_list[j] = my_list[j-1]
+            my_list[j-1] = temp
+    return insertion_sort_recursive(my_list[:-1]) + my_list[-1:]
 
 
 my_list = [4,2,6,5,1,3]
